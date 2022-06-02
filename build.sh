e04fc02d53978afc493f3bb8b18f3f2d5ea355b1
lyswifter <liuyong@tianrukj.com>
2022-06-02 16:25:28 +0800

    add canister_ids.json

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 cargo build --target wasm32-unknown-unknown --release
 ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/manage.wasm -o ./target/wasm32-unknown-unknown/release/manage.wasm && \ 
 
+ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/test_canister.wasm -o ./target/wasm32-unknown-unknown/release/test_canister.wasm && \
 
-ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/test_canister.wasm -o ./target/wasm32-unknown-unknown/release/test_canister.wasm && \ 
-
+ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/image_store.wasm -o ./target/wasm32-unknown-unknown/release/image_store.wasm && \
 
 ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/canister_log.wasm -o ./target/wasm32-unknown-unknown/release/canister_log.wasm
 
